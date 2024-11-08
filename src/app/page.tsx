2b3bfc9aa8ef217f556a05a24285ae4523a13cad
christopher uchenna <uchennachris020@gmail.com>
2024-11-08 12:15:16 +0100

    last changes made to landing page done. Added the content and Hero section Image while also making it mobile responsive.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,62 @@
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
+import Balancer from "react-wrap-balancer";
+
+// images
+import databiz from "@/assets/images/client-databiz.svg";
+import audiophile from "@/assets/images/client-audiophile.svg";
+import meet from "@/assets/images/client-meet.svg";
+import maker from "@/assets/images/client-maker.svg";
+
+import hremoImagedesktop from "@/assets/images/image-hero-desktop.png";
+import hremoImageMobile from "@/assets/images/image-hero-mobile.png";
 
 export default function Home() {
+  const bannerImage = [databiz, audiophile, meet, maker];
+
   return (
     <div className="min-h-screen w-full bg-gray-100">
       <Navbar />
       <section className="mx-auto flex max-w-6xl flex-col-reverse gap-2 px-4 pb-12 transition-all md:flex-row md:gap-4">
         {/* left div */}
         <div className="flex flex-col items-center gap-6 pt-8 text-center md:w-1/2 md:items-start md:gap-10 md:pt-32 md:text-left">
-          <h1 clasaName></h1>
+          <h1 className="text-4xl font-semibold md:text-6xl">
+            <Balancer>Make Remote Work</Balancer>
+          </h1>
+          <p className="text-neutral-400  md:max-w-[400px]">
+            <Balancer>
+              Get your team in sync no matter your location. streamline
+              processes, create team rituals, and watch productivity soar.
+            </Balancer>
+          </p>
+          <button className="border-black w-fit rounded-xl border-2 bg-black px-4 py-2 text-white transition-all hover:border-black hover:bg-black hover:bg-transparent hover:text-black/90">
+            Learn More
+          </button>
+          <div className="flex gap-2 md:gap-6">
+            {bannerImage.map((img, i) => (
+              <Image
+                className="h-5 w-auto"
+                key={i}
+                src={img}
+                alt="Client Image"
+              />
+            ))}
+          </div>
         </div>
+
+        {/* right div */}
+        <section className="md:w-1/2 ">
+          <Image
+            className="hidden h-auto max-w-[400px]  md:block"
+            src={hremoImagedesktop}
+            alt="hreo-image"
+          />
+          <Image
+            className="h-auto w-full  md:hidden"
+            src={hremoImageMobile}
+            alt="hreo-image"
+          />
+        </section>
       </section>
     </div>
   );
